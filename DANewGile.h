753518044c3cb1a1d98198c0b68355ed0384a4a7
darnoldi <davearnoldi@Daves-iMac.local>
2014-08-28 18:37:06 +0200

    added test class

---
+++ DANewGile.h
@@ -0,0 +1,13 @@
+//
+//  DANewGile.h
+//  Git Test
+//
+//  Created by Dave Arnoldi on 2014/08/28.
+//  Copyright (c) 2014 Dave Arnoldi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DANewGile : NSObject
+
+@end
---
+++ DANewGile.m
@@ -0,0 +1,13 @@
+//
+//  DANewGile.m
+//  Git Test
+//
+//  Created by Dave Arnoldi on 2014/08/28.
+//  Copyright (c) 2014 Dave Arnoldi. All rights reserved.
+//
+
+#import "DANewGile.h"
+
+@implementation DANewGile
+
+@end
